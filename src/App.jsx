fb113e4785bc963837912772579d044dc0994de3
Kaycee34 <udechukwudestinykelechi945@gmail.com>
2024-11-22 11:41:46 +0100

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Navbar from "./components/navbar";
+
 function App() {
   return (
     <>
       <h1 className="text-red-500">Aba Shirts e-commerce website</h1>
+
+      <Navbar />
     </>
   );
 }
